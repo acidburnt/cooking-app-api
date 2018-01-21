b6a713309face4b9a7bb1c56a82637c0fc5a9f6e
acidburnt <locky700@gmail.com>
2018-01-21 22:27:09 +0100

    add post login path

--- src/models/User.js
+++ src/models/User.js
@@ -66,6 +66,25 @@ UserSchema.statics.findByToken = function findByToken(token) {
     'tokens.access': 'auth',
   });
 };
+UserSchema.statics.findByCredentials = function findByCredentials(email, password) {
+  const User = this;
+  return User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+      return new Promise((resolve, reject) => {
+
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject();
+          }
+        });
+      });
+    });
+};
 
 UserSchema.pre('save', function crypt(next) {
   const user = this;
