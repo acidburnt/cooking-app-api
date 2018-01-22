fa468d6d3cc96f571e5e7d8294742f1647c3c241
acidburnt <locky700@gmail.com>
2018-01-22 04:21:13 +0100

    added cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { mongoose } = require('./db/mongoose');
 
@@ -9,7 +10,7 @@ const port = process.env.PORT;
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 // Routes for Recipes
 require('./routes/recipe')(app);
 
